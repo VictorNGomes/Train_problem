fd4b64127e8e1f0229fb8b81cfbffcba67b521bb
Victor Gomes <victorgomes.vng@hotmail.com>
2023-12-11 11:57:32 -0300

    slides ok ainda com deadlock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import threading
 import time
+import pygame_gui
 
 # Inicialização do Pygame
 pygame.init()
@@ -156,6 +157,23 @@ def draw_trains(trains):
 
     pygame.display.flip()
 
+def initialize_gui(manager, trains):
+    font = pygame.font.Font(None, 36)
+    screen_width, _ = pygame.display.get_surface().get_size()
+
+    for i, train in enumerate(trains):
+        slider_rect = pygame.Rect(width - 210, 10 + i * 50, 200, 20)
+        slider = pygame_gui.elements.UIHorizontalSlider(slider_rect, 1, (1, 50), manager=manager)
+        #slider.set_value(train.speed)  # Set the initial value of the slider
+
+        # Adjust X-coordinates to position the slider and label to the right
+        label_rect = pygame.Rect(width - 420, 10 + i * 50, 200, 50)
+        label = pygame_gui.elements.UILabel(relative_rect=label_rect,
+                                             text=f"Speed {train.name}: {train.speed}", manager=manager)
+
+        train.slider = slider
+        train.label = label
+
 # Função principal
 def main():
     # Definindo rotas
@@ -187,13 +205,32 @@ def main():
     thread_train4.start()
 
     # Loop principal
+    pygame.init()
+    pygame.display.set_caption("Simulação de Trens")
+    clock = pygame.time.Clock()
+    manager = pygame_gui.UIManager((width, height))
+
+    initialize_gui(manager, [train1, train2, train3, train4])
+
+    # Loop principal
     while True:  
+        time_delta = clock.tick(60) / 1000.0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        draw_trains([train1, train2, train3, train4])
+            manager.process_events(event)
 
+        for train in [train1, train2, train3, train4]:
+            train.speed = int(train.slider.get_current_value())
+            train.label.set_text(f"Speed {train.name}: {train.speed}")
+
+        manager.update(time_delta)
+        draw_trains([train1, train2, train3, train4])
+        manager.draw_ui(screen)
+
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
